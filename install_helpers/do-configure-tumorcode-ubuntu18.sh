c5e27ff1f1af1669982f33c6d6734aa80a0f2d69
Ubuntu <ubuntu@ip-172-31-23-173.us-west-2.compute.internal>
2022-07-25 19:26:49 +0000

    Testing new install helpers

--- install_helpers/do-configure-tumorcode-ubuntu18.sh
+++ install_helpers/do-configure-tumorcode-ubuntu18.sh
@@ -1,8 +1,11 @@
+TRILINOSLIBS="ml;epetra;epetraext;amesos;aztecoo;ifpack;\
+teuchosparameterlist;teuchosremainder;teuchosnumerics;\
+teuchoscomm;teuchoscore"
 cmake \
+    -DTrilinos_DIR:PATH=/localdisk/trilinos12/lib/cmake/Trilinos \
+    -DTrilinos_LIBRARIES:STRING=$TRILINOSLIBS \
     -DCMAKE_INSTALL_PREFIX=/tc_app/tc_install \
     -DCMAKE_BUILD_TYPE=Release \
     -DADDITIONAL_INCLUDE_DIRS="" \
     -DADDITIONAL_LIBRARY_DIRS="" \
-    -DHDF5_INCLUDE_DIRS="/usr/include/hdf5/openmpi" \
-    -DCMAKE_CXX_COMPILER="/usr/bin/mpic++" \
     $1
